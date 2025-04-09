a2e335e20deab13f38eea12d37f13c0f1252bbf5
ddobryak <ddobryak@gmail.com>
2025-04-08 23:10:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 import { STLLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/STLLoader.js';
